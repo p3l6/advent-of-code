cee6aca1a519f7fb8886f581c3384ebe7b33ec1b
Paul Landers <paul.landers@testplant.com>
2017-12-04 00:43:39 -0700

    Day 4

--- AoC/main.swift
+++ AoC/main.swift
@@ -8,129 +8,54 @@
 import Foundation
 
 
-let input = 265149
+let input = puzzleInput
+"""
+cat ma tac tca
+cat tra rat
+cat dog
+"""
 
-var x = 0
-var y = 0
-var band = 0
+let phrases = input.split(separator: "\n")
+var validCount = 0;
 
-for i in 1..<input {
-    
-    //print("\(i) is (\(x),\(y))")
-    
-    let bandMax = squared(band*2+1)
-    let width = band*2+1
-    let lastMax = squared((max(band,1)-1)*2+1)
-    let bandIndex = i - lastMax
-    
-    if bandMax == i {
-        band += 1
-        x += 1
-    } else if bandIndex < width-1 {
-        y += 1
-    } else if bandIndex < 2*(width-1) {
-        x -= 1
-    } else if bandIndex < 3*(width-1) {
-        y -= 1
-    } else  {
-        x += 1
-    }
-    
-}
-
-
-//print("\(input) is (\(x),\(y))")
-
-print("part one: \(abs(x)+abs(y))") // 438
-
-
-
-var values = [1]
-
-func indexOfCoord(_ xx :Int, _ yy: Int) -> Int {
-    var x = 0
-    var y = 0
-    var band = 0
-    
-    for i in 1... {
-        
-        if x == xx && y == yy {
-            return i-1
-        }
-        
-        let bandMax = squared(band*2+1)
-        let width = band*2+1
-        let lastMax = squared((max(band,1)-1)*2+1)
-        let bandIndex = i - lastMax
-        
-        if bandMax == i {
-            band += 1
-            x += 1
-        } else if bandIndex < width-1 {
-            y += 1
-        } else if bandIndex < 2*(width-1) {
-            x -= 1
-        } else if bandIndex < 3*(width-1) {
-            y -= 1
-        } else  {
-            x += 1
+for phrase in phrases {
+    var wordMap = [String:Int]()
+    var valid = true
+    for word in phrase.split(separator: " ") {
+        if let _ = wordMap[String(word)] {
+            valid = false
+            break
+        } else {
+            wordMap[String(word)] = 1
         }
     }
-    return 0
+    if valid {
+        validCount+=1
+    }
 }
 
-func valueAt(_ x :Int, _ y: Int) -> Int {
-    let index = indexOfCoord(x, y)
-    if index >= values.count {
-        return 0
+print("part one: \(validCount)") // 455
+
+validCount = 0;
+
+for phrase in phrases {
+    var wordMap = [String:Int]()
+    var valid = true
+    for word in phrase.split(separator: " ") {
+        let key = word.utf8.sorted().map({"\($0)"}).joined()
+            // this ended up being a string of numbers representing the letters.
+        if let _ = wordMap[key] {
+            valid = false
+            break
+        } else {
+            wordMap[key] = 1
+        }
     }
-    return values[index]
+    if valid {
+        validCount+=1
+    }
 }
 
-
-x = 0
-y = 0
-band = 0
-
-for i in 1... {
-    
-    let bandMax = squared(band*2+1)
-    let width = band*2+1
-    let lastMax = squared((max(band,1)-1)*2+1)
-    let bandIndex = i - lastMax
-    
-    if bandMax == i {
-        band += 1
-        x += 1
-    } else if bandIndex < width-1 {
-        y += 1
-    } else if bandIndex < 2*(width-1) {
-        x -= 1
-    } else if bandIndex < 3*(width-1) {
-        y -= 1
-    } else  {
-        x += 1
-    }
-    
-    // calculate value for these new coords
-    var value = 0
-    value += valueAt(x-1, y-1)
-    value += valueAt(x-1, y  )
-    value += valueAt(x-1, y+1)
-    value += valueAt(x  , y-1)
-//  value += valueAt(x  , y  )  this square
-    value += valueAt(x  , y+1)
-    value += valueAt(x+1, y-1)
-    value += valueAt(x+1, y  )
-    value += valueAt(x+1, y+1)
-    
-    values.append(value)
-    //print("\(value) is (\(x),\(y))")
-    if value > input {
-        print("part two: \(value)") // 226330
-        break
-    }
-   
-}
+print("part two: \(validCount)") // 186
 
 
