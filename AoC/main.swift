3a7be32d51cc79b51893c89c5776108405010629
Paul Landers <paul.landers@testplant.com>
2018-01-09 14:43:10 -0700

    Day 25.

--- AoC/main.swift
+++ AoC/main.swift
@@ -9,85 +9,45 @@ import Foundation
 let execTime = TicToc(named:"Today's problems")
 
 let input = puzzleInput
-"""
-0/2
-2/2
-2/3
-3/4
-3/5
-0/1
-10/1
-9/10
-"""
 
-struct Component {
-    let a : Int
-    let b : Int
-    var strength : Int { return a + b }
-    func reversed() -> Component { return Component(a:b, b:a) }
+struct State {
+    struct Ops {
+        let write :Int
+        let dIndex :Int
+        let dState :String
+    }
+    let zero :Ops
+    let one :Ops
 }
 
-struct Bridge : CustomStringConvertible {
-    let used : [Component]
-    let unused : [Component]
-    
-    var strength : Int { var s = 0; used.forEach({s+=$0.strength}); return s }
-    var length : Int { return used.count }
-    
-    func extensions() -> [Bridge]{
-        var ret = [Bridge]()
-        let next = used.isEmpty ? 0 : used.last!.b
-        for (i,com) in unused.enumerated()  {
-            if com.a == next {
-                var remaining = unused
-                remaining.remove(at: i)
-                ret.append(Bridge(used: used+[com],
-                                  unused: remaining))
-            } else if com.b == next {
-                var remaining = unused
-                remaining.remove(at: i)
-                ret.append(Bridge(used: used+[com.reversed()],
-                                  unused: remaining))
-            }
-        }
-        return ret
-    }
-    
-    var description: String {
-        var s = ""; used.forEach({s+="\($0.a)/\($0.b)--"}); return s
-    }
-}
+let states = [
+    "A":State(zero:State.Ops(write: 1, dIndex: +1, dState: "B"), one:State.Ops(write: 0, dIndex: -1, dState: "C")),
+    "B":State(zero:State.Ops(write: 1, dIndex: -1, dState: "A"), one:State.Ops(write: 1, dIndex: +1, dState: "D")),
+    "C":State(zero:State.Ops(write: 1, dIndex: +1, dState: "A"), one:State.Ops(write: 0, dIndex: -1, dState: "E")),
+    "D":State(zero:State.Ops(write: 1, dIndex: +1, dState: "A"), one:State.Ops(write: 0, dIndex: +1, dState: "B")),
+    "E":State(zero:State.Ops(write: 1, dIndex: -1, dState: "F"), one:State.Ops(write: 1, dIndex: -1, dState: "C")),
+    "F":State(zero:State.Ops(write: 1, dIndex: +1, dState: "D"), one:State.Ops(write: 1, dIndex: +1, dState: "A")),
+]
+var state = "A"
 
-let emptyBridge = Bridge(used:[],
-                         unused:input.split(separator: "\n").map({
-                            let s = String($0).integerArray("/");
-                            return Component(a:s[0], b:s[1])
-                         }))
+var oneBits = Set<Int>()
+var cursor = 0
 
-var eval = Stack<Bridge>()
-eval.push(emptyBridge)
-var strongest = emptyBridge
-var longest = emptyBridge
-
-while !eval.isEmpty {
-    let bridge = eval.pop()
-   
-   // print (bridge)
-    let ext = bridge.extensions()
-    if ext.isEmpty {
-        if bridge.strength > strongest.strength {
-            strongest = bridge
-        }
-        if bridge.length > longest.length || bridge.length == longest.length && bridge.strength > longest.strength {
-            longest = bridge
-        }
+for _ in 0 ..< 12919244 {
+    let ops = oneBits.contains(cursor) ? states[state]!.one : states[state]!.zero
+    
+    if ops.write == 1 {
+        oneBits.insert(cursor)
     } else {
-        ext.forEach { eval.push($0) }
+        oneBits.remove(cursor)
     }
+    
+    cursor += ops.dIndex
+    state = ops.dState
 }
 
-print("part one: \(strongest.strength)") // 2006
 
-print("part two: \(longest.strength)")  // 1994 
+print("part one: \(oneBits.count)") // 4287
+print("part two: N/A")  //
 
 execTime.end()
