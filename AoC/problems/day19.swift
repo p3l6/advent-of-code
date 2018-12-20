7fe77630883e0dbc5f0f31028bad582d6d7960a7
Paul Landers <paul@srednal.com>
2018-12-20 00:08:51 -0700

    Day 19 pt 2

--- AoC/problems/day19.swift
+++ AoC/problems/day19.swift
@@ -7,60 +7,23 @@
 
 import Foundation
 
-let runDay19 = true
+let runDay19 = false
+
+func sumOfDivisors(_ f:Int) -> Int {
+    var a = 0
+    for x in 1...f {
+        if f == (f/x) * x {
+            a += x
+        }
+    }
+    return a
+}
 
 func day19 (_ input:String) -> Solution {
-    
-    var state = DeviceState(registers:[0,0,0,0,0,0])
-    let ipIndex = input.lines().first!.extract(format: "#ip %")!.first!
-    let program = input.lines()[1...].map { line -> (OpCode,Int,Int,Int) in
-        var op = OpCode.unknown
-        switch line.substring(toIndex: 4){
-        case "addr": op = .addr
-        case "addi": op = .addi
-        case "mulr": op = .mulr
-        case "muli": op = .muli
-        case "banr": op = .banr
-        case "bani": op = .bani
-        case "borr": op = .borr
-        case "bori": op = .bori
-        case "setr": op = .setr
-        case "seti": op = .seti
-        case "gtir": op = .gtir
-        case "gtri": op = .gtri
-        case "gtrr": op = .gtrr
-        case "eqir": op = .eqir
-        case "eqri": op = .eqri
-        case "eqrr": op = .eqrr
-        default: assertionFailure("unreadable line: \(line)")
-        }
-        let ints = line.substring(fromIndex: 4).integerArray(" ")
-        return (op, ints[0], ints[1], ints[2])
-    }
-    
-    var ip = state.registers[ipIndex]
-    while ip < program.count {
-        let op = program[ip]
-        state = state.stateBySettingRegister(at: ipIndex, to: ip)
-        state = state.perform(opCode: op.0, inputs: (op.1,op.2), output: op.3)
-        ip = state.registers[ipIndex]
-        ip += 1
-    }
-    
-    
     var solution = Solution()
-    solution.partOne = "\(state.registers[0])"
     
-    state = DeviceState(registers:[1,0,0,0,0,0])
-    ip = state.registers[ipIndex]
-    while ip < program.count {
-        let op = program[ip]
-        state = state.stateBySettingRegister(at: ipIndex, to: ip)
-        state = state.perform(opCode: op.0, inputs: (op.1,op.2), output: op.3)
-        ip = state.registers[ipIndex]
-        ip += 1
-    }
-    
-    solution.partTwo = "\(state.registers[0])"
+    // This algorithm and inputs were found by manually "disassembling" the input on paper!
+    solution.partOne = "\(sumOfDivisors(836 + 119))"
+    solution.partTwo = "\(sumOfDivisors(836 + 119 + ((27*28)+29)*30*14*32))"
     return solution
 }
