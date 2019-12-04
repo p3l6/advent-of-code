bd2110f5e92a7eb9a82dd07628c0af92ddfd204a
Paul Landers <planders@statestitle.com>
2019-12-03 21:54:41 -0500

    Day 3

--- AoC/problems/day3.swift
+++ AoC/problems/day3.swift
@@ -9,9 +9,56 @@ import Foundation
 
 let runDay3 = false
 
+func getGrid(_ strPath:String) -> InfiniteGrid<Int> {
+    let path = InfiniteGrid(defaultValue: 0)
+    var pos = Point(0,0)
+    var steps = 0
+    for seg in strPath.stringArray(",") {
+        let dir :Direction = { () -> Direction in
+            switch seg.first! {
+            case "R": return Direction.east
+            case "L": return Direction.west
+            case "D": return Direction.south
+            case "U": return Direction.north
+            default:
+                print("unexpected direction!")
+                abort()
+            }
+        }()
+        let dist = Int(seg.substring(fromIndex: 1))!
+        for _ in 0..<dist {
+            pos = pos.move(dir)
+            steps += 1
+            path[pos] = steps
+        }
+    }
+    return path
+}
+
 func day3 (_ input:String) -> Solution {
-    let solution = Solution()
-//    solution.partOne = "\()"
-//    solution.partTwo = "\()"
+    var solution = Solution()
+    let lines = input.lines()
+    
+    let wire1path = getGrid(lines[0])
+    let wire2path = getGrid(lines[1])
+    
+    var minDist = Int.max
+    var minDelay = Int.max
+    wire1path.forEach { (p, steps1) in
+        let steps2 = wire2path[p]
+        if steps1 > 0 && steps2 > 0 {
+            let dist = p.taxi(to: Point(0,0))
+            if dist < minDist {
+                minDist = dist
+            }
+            if steps1 + steps2 < minDelay {
+                minDelay = steps1 + steps2
+            }
+
+        }
+    }
+    
+    solution.partOne = "\(minDist)"
+    solution.partTwo = "\(minDelay)"
     return solution
 }
--- AocTest/AocTest.swift
+++ AocTest/AocTest.swift
@@ -36,8 +36,20 @@ class Day1Tests: AocTest { override var problem:Problem{return day1}; override v
 """, p1:"658", p2:"970"),
     ]}}
 //class Day2Tests: AocTest { override var problem:Problem{return day2}; override var examples :[Case] { return []}}
-//class Day3Tests: AocTest { override var problem:Problem{return day3}; override var examples :[Case] { return [
-//    ]}}
+class Day3Tests: AocTest { override var problem:Problem{return day3}; override var examples :[Case] { return [
+    Case(input:"""
+    R8,U5,L5,D3
+    U7,R6,D4,L4
+    """, p1:"6", p2:"30"),
+    Case(input:"""
+    R75,D30,R83,U83,L12,D49,R71,U7,L72
+    U62,R66,U55,R34,D71,R55,D58,R83
+    """, p1:"159", p2:"610"),
+    Case(input:"""
+    R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51
+    U98,R91,D20,R16,D67,R40,U7,R15,U6,R7
+    """, p1:"135", p2:"410"),
+    ]}}
 //class Day4Tests: AocTest { override var problem:Problem{return day4}; override var examples :[Case] { return [
 //    ]}}
 //class Day5Tests: AocTest { override var problem:Problem{return day5}; override var examples :[Case] { return [
