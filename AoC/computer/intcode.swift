31a345b2326230919b4f88e0363defd1db5a4374
Paul Landers <planders@statestitle.com>
2019-12-05 18:19:29 -0500

    Keep first intcode, refactor existing

--- AoC/computer/intcode.swift
+++ AoC/computer/intcode.swift
@@ -7,28 +7,36 @@
 
 import Foundation
 
-func intcodeProg(_ program: [Int]) -> Int {
+class Intcode {
     enum OpCode: Int {
         case Add = 1
         case Mult = 2
         case Halt = 99
     }
-    var prog = program
-    var pc = 0
-    while prog[pc] != OpCode.Halt.rawValue {
-        let a = prog[prog[pc+1]]
-        let b = prog[prog[pc+2]]
-        let outPos = prog[pc+3]
-        switch prog[pc] {
-        case OpCode.Add.rawValue:
-            prog[outPos] = a + b
-        case OpCode.Mult.rawValue:
-            prog[outPos] = a * b
-        default:
-            print("Unexpected opcode")
-            exit(1)
-        }
-        pc += 4
+    
+    var prog :[Int]
+    var ip = 0
+    
+    init(program: [Int]) {
+        self.prog = program
     }
-    return prog.first!
+    
+    func run() -> Int {
+        while prog[ip] != OpCode.Halt.rawValue {
+            let a = prog[prog[ip+1]]
+            let b = prog[prog[ip+2]]
+            let outPos = prog[ip+3]
+            switch prog[ip] {
+            case OpCode.Add.rawValue:
+                prog[outPos] = a + b
+            case OpCode.Mult.rawValue:
+                prog[outPos] = a * b
+            default:
+                print("Unexpected opcode")
+                exit(1)
+            }
+            ip += 4
+        }
+        return prog.first!
+    }
 }
