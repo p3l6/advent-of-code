040de8632bc2b80da77860cc8993bd3c949f7073
Paul Landers <planders@statestitle.com>
2019-12-08 21:42:30 -0500

    Day 8!

--- AoC/problems/day8.swift
+++ AoC/problems/day8.swift
@@ -10,8 +10,51 @@ import Foundation
 let runDay8 = false
 
 func day8 (_ input:String) -> Solution {
-    let solution = Solution()
-//    solution.partOne = "\()"
-//    solution.partTwo = "\()"
+    var solution = Solution()
+    let imageSize = (width: 25, height: 6)
+    let imageData :[Int] = input.map { Int(String($0))! }
+    let pixelsPerLayer = imageSize.width * imageSize.height
+    let numLayers = imageData.count / pixelsPerLayer
+    
+    if imageData.count % pixelsPerLayer != 0 {
+        print("Image layers are not even!")
+        exit(1)
+    }
+    
+    enum Pixel: Int {
+        case Transparent = 2
+        case White = 1
+        case Black = 0
+    }
+    
+    var image = [Pixel](repeating: Pixel.Transparent, count: pixelsPerLayer)
+    
+    var min = (fewestZero: Int.max, mult: 0)
+    for l in 0..<numLayers {
+        var counts = [0,0,0]
+        for p in 0..<pixelsPerLayer {
+            let color = Pixel(rawValue:imageData[l*pixelsPerLayer+p])!
+            counts[color.rawValue] += 1
+            if image[p] == .Transparent && color != .Transparent {
+                image[p] = color
+            }
+        }
+        if counts[0] < min.fewestZero {
+            min = (counts[0], counts[1] * counts[2])
+        }
+    }
+    
+    solution.partOne = "\(min.mult)"
+    
+    print("Part two rendering:")
+    for row in 0..<imageSize.height {
+        for col in 0..<imageSize.width {
+            print(image[row*imageSize.width+col] == .White ? "X" : " ", terminator:"")
+        }
+        print("")
+    }
+    
+    // Manually read from output
+    solution.partTwo = "RKHRY"
     return solution
 }
