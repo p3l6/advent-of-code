9c160478da58ef4bd9314efca6b6ea9188921ad2
Paul Landers <planders@statestitle.com>
2020-12-07 10:10:15 -0500

    day 7

--- 2020/AoC/problems/day7.swift
+++ 2020/AoC/problems/day7.swift
@@ -3,10 +3,75 @@ import Foundation
 
 let runDay7 = false
 
+class BagType {
+    let color: String
+    let contains: [(String, Int)]
+    let containedCount: Int
+    var hasShinyGold: Bool?
+    
+    init(data: String) {
+        let words = data.stringArray(" ")
+        color = words[0...1].joined(separator: " ")
+        
+        var contains = [(String,Int)]()
+        if words[4] != "no" {
+            var i = 0
+            repeat {
+                i += 4
+                contains.append((words[i+1...i+2].joined(separator: " "), Int(words[i])!))
+            } while words[i+3].last! != "."
+        }
+        
+        self.contains = contains
+        self.containedCount = contains.map { $0.1 }.sum()
+    }
+}
+
 func day7 (_ input:String) -> Solution {
-    let solution = Solution()
-//    solution.partOne = "\()"
-//    solution.partTwo = "\()"
+    var solution = Solution()
+    let lines = input.lines()
+    
+    let bagMap: [String: BagType] = lines.map { BagType(data: $0) }.reduce(into: Dictionary()) { $0[$1.color] = $1 }
+    
+    func findShinyGold(inside bag: BagType) -> Bool {
+        for (contained, _) in bag.contains {
+            let cBag = bagMap[contained]!
+            if  contained == "shiny gold" ||
+                (cBag.hasShinyGold != nil && cBag.hasShinyGold!) ||
+                (cBag.hasShinyGold == nil && findShinyGold(inside: cBag)) {
+                bag.hasShinyGold = true
+                return true
+            }
+        }
+        bag.hasShinyGold = false
+        return false
+    }
+    
+    for (_, bag) in bagMap {
+        if bag.hasShinyGold == nil {
+            let _ = findShinyGold(inside: bag)
+        }
+    }
+    
+    solution.partOne = "\(bagMap.map{$0.value.hasShinyGold!}.count(where:{$0}))"
+    
+    
+    var bagsToCheck = [(color: "shiny gold", multiplier:1)]
+    var totalBags = 0
+
+    while !bagsToCheck.isEmpty {
+        let (color, mult) = bagsToCheck.popLast()!
+        let bag = bagMap[color]!
+        
+        totalBags += bag.containedCount * mult
+        
+        for (color, amount) in bag.contains {
+            bagsToCheck.append((color: color, multiplier: amount * mult))
+        }
+    }
+    
+    
+    solution.partTwo = "\(totalBags)"
     return solution
 }
 
--- 2020/AocTest/AocTest.swift
+++ 2020/AocTest/AocTest.swift
@@ -114,10 +114,28 @@ b
 """, p1:"11", p2:"6"),
 ]}}
 
-// class Day7Tests: AocTest { override var problem:Problem{return day7}; override var examples :[Case] { return [
-// Case(input:"""
-// """, p1:"", p2:""),
-// ]}}
+class Day7Tests: AocTest { override var problem:Problem{return day7}; override var examples :[Case] { return [
+Case(input:"""
+light red bags contain 1 bright white bag, 2 muted yellow bags.
+dark orange bags contain 3 bright white bags, 4 muted yellow bags.
+bright white bags contain 1 shiny gold bag.
+muted yellow bags contain 2 shiny gold bags, 9 faded blue bags.
+shiny gold bags contain 1 dark olive bag, 2 vibrant plum bags.
+dark olive bags contain 3 faded blue bags, 4 dotted black bags.
+vibrant plum bags contain 5 faded blue bags, 6 dotted black bags.
+faded blue bags contain no other bags.
+dotted black bags contain no other bags.
+""", p1:"4", p2:"32"),
+Case(input:"""
+shiny gold bags contain 2 dark red bags.
+dark red bags contain 2 dark orange bags.
+dark orange bags contain 2 dark yellow bags.
+dark yellow bags contain 2 dark green bags.
+dark green bags contain 2 dark blue bags.
+dark blue bags contain 2 dark violet bags.
+dark violet bags contain no other bags.
+""", p1:"", p2:"126"),
+]}}
 
 // class Day8Tests: AocTest { override var problem:Problem{return day8}; override var examples :[Case] { return [
 // Case(input:"""
