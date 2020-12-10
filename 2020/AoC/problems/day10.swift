ea38aa827e39fbf8e7b71670f8d114b49861242a
Paul Landers <planders@statestitle.com>
2020-12-10 10:09:53 -0500

    day 10

--- 2020/AoC/problems/day10.swift
+++ 2020/AoC/problems/day10.swift
@@ -4,9 +4,44 @@ import Foundation
 let runDay10 = false
 
 func day10 (_ input:String) -> Solution {
-    let solution = Solution()
-//    solution.partOne = "\()"
-//    solution.partTwo = "\()"
+    var solution = Solution()
+    let adapters = input.integerArray("\n").sorted()
+    
+    let differences: [Int] = adapters
+        .enumerated()
+        .map { (idx, adapter) in
+        if idx == 0 { return adapter }
+        return adapter - adapters[idx-1]
+    }
+    
+    let product = differences.count(where: {$0 == 1}) * (differences.count(where: { $0 == 3 }) + 1)
+    solution.partOne = "\(product)"
+    
+    var cache = [ArraySlice<Int>:Int]()
+    func countSubLists(of list: ArraySlice<Int>) -> Int {
+        if list.count == 1 {
+            return 1
+        } else if let count = cache[list] {
+            return count
+        }
+        
+        var count = 0
+        let offset = list.startIndex
+        for i in offset+1 ..< min(offset+4, list.endIndex) {
+            if list[i] - list[offset] <= 3 {
+                count += countSubLists(of: list[i...])
+            }
+        }
+        cache[list] = count
+        return count
+    }
+    
+    var valids = countSubLists(of: adapters[0...])
+    if adapters[1] <= 3 { valids += countSubLists(of: adapters[1...]) }
+    if adapters[2] <= 3 { valids += countSubLists(of: adapters[2...]) }
+    
+    solution.partTwo = "\(valids)"
+    
     return solution
 }
 
--- 2020/AocTest/AocTest.swift
+++ 2020/AocTest/AocTest.swift
@@ -156,10 +156,54 @@ acc +6
 // """, p1:"", p2:""),
 // ]}}
 
-// class Day10Tests: AocTest { override var problem:Problem{return day10}; override var examples :[Case] { return [
-// Case(input:"""
-// """, p1:"", p2:""),
-// ]}}
+class Day10Tests: AocTest { override var problem:Problem{return day10}; override var examples :[Case] { return [
+Case(input:"""
+16
+10
+15
+5
+1
+11
+7
+19
+6
+12
+4
+""", p1:"35", p2:"8"),
+Case(input:"""
+28
+33
+18
+42
+31
+14
+46
+20
+48
+47
+24
+23
+49
+45
+19
+38
+39
+11
+1
+32
+25
+35
+8
+17
+7
+9
+4
+2
+34
+10
+3
+""", p1:"220", p2:"19208"),
+]}}
 
 // class Day11Tests: AocTest { override var problem:Problem{return day11}; override var examples :[Case] { return [
 // Case(input:"""
