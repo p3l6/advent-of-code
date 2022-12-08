da9436bd0436ca67fa419cd135e8fdc94f45d37c
Paul Landers <paul@srednal.com>
2022-12-08 13:50:35 -0500

    8 pt 2

--- 2022/deno/bin/day8.ts
+++ 2022/deno/bin/day8.ts
@@ -6,7 +6,7 @@ runDay(8, day, [{
 65332
 33549
 35390`,
-  answer: 21,
+  answer: [21, 8],
 }]);
 
 function day(input: string): Answer {
@@ -27,7 +27,7 @@ function day(input: string): Answer {
     let max = grid[row][0];
     for (let col = 1; col < cols - 1; col++) if (max < grid[row][col]) {max = grid[row][col]; visibleTrees[row][col] = true;}
     max = grid[row][cols-1];
-    for (let col = cols-2; col >= 1; col--)  if (max < grid[row][col]) {max = grid[row][col]; visibleTrees[row][col] = true; }
+    for (let col = cols-2; col >= 1; col--)  if (max < grid[row][col]) {max = grid[row][col]; visibleTrees[row][col] = true;}
   }
 
   for (let col = 1; col < grid[0].length - 1; col++) {
@@ -35,14 +35,24 @@ function day(input: string): Answer {
     visibleTrees[rows-1][col] = true;
 
     let max = grid[0][col];
-    for (let row = 1; row < rows - 1; row++) if (max < grid[row][col]) {max = grid[row][col]; visibleTrees[row][col] = true; }
+    for (let row = 1; row < rows - 1; row++) if (max < grid[row][col]) {max = grid[row][col]; visibleTrees[row][col] = true;}
     max = grid[rows-1][col];
-    for (let row = rows-2; row >= 1; row--)  if (max < grid[row][col]) {max = grid[row][col]; visibleTrees[row][col] = true; }
+    for (let row = rows-2; row >= 1; row--)  if (max < grid[row][col]) {max = grid[row][col]; visibleTrees[row][col] = true;}
   }
 
-  // console.log(visibleTrees)
-  const partOne = visibleTrees.flat().reduce((t,x)=>x?t+1:t, 0);
-  return partOne;
-  const partTwo = 0;
+  const partOne = visibleTrees.flat().reduce((t,x)=>x?t+1:t, 0); // sum()
+
+  const scenicScores = emptyGrid(0, grid.length, grid[0].length);
+  for (let row = 1; row < grid.length - 1; row++) {
+    for (let col = 1; col < grid[0].length - 1; col++) {
+      let visRight = 1; while (col+visRight+1 < cols && grid[row][col] > grid[row][col+visRight]) visRight++;
+      let visLeft  = 1; while (col-visLeft-1  >= 0   && grid[row][col] > grid[row][col-visLeft])  visLeft++;
+      let visUp    = 1; while (row-visUp-1    >= 0   && grid[row][col] > grid[row-visUp][col])    visUp++;
+      let visDown  = 1; while (row+visDown+1  < rows && grid[row][col] > grid[row+visDown][col])  visDown++;
+      scenicScores[row][col] = visRight * visLeft * visUp * visDown;
+    }
+  }
+
+  const partTwo = scenicScores.flat().reduce((a,b)=>a>b?a:b, 0); // max()
   return [partOne, partTwo];
 }
