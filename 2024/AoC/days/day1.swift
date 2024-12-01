240a27a2b11f46d57e5e963d799349581b967e04
Paul Landers <paul@thelanders.family>
2024-12-01 14:46:04 -0500

    2024 day 1

--- 2024/AoC/days/day1.swift
+++ 2024/AoC/days/day1.swift
@@ -6,12 +6,37 @@ import Shared
     let dayNumber = 1
     let tests = [
         TestCase("""
- """, .one(6))
+ 3   4
+ 4   3
+ 2   5
+ 1   3
+ 3   9
+ 3   3
+ """, .one(11) + .two(31))
     ]
 }
 
 func day(_ input: Input) -> Answer {
+    let (a, b): ([Int], [Int]) = input.lines
+        .map { $0.split(separator: "   ") }
+        .reduce(into: ([], [])) { $0.0.append(Int($1.first!)!); $0.1.append(Int($1.last!)!) }
 
-    return .one(5) //  + .two(7)
+    let aSorted = a.sorted()
+    let bSorted = b.sorted()
+
+    var difference = 0
+
+    for (x, y) in zip(aSorted, bSorted) {
+        difference += abs(x-y)
+    }
+
+    let bIndex = b.reduce(into: [:]) { $0[$1] = ($0[$1] ?? 0) + 1 }
+    var similarity = 0
+
+    for x in aSorted {
+        similarity += x * (bIndex[x] ?? 0)
+    }
+
+    return .one(difference) + .two(similarity)
 }
 
